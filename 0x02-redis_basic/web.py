c443151c2b9c1149c460d0f64e50cf1cc59a0fb7
Moses <akojimoses@gmail.com>
2023-02-19 23:42:33 +0000

    Redis

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+def access(method: Callable) -> Callable:
+    """decorator for get_page"""
+    @wraps(method)
+    def count(url: str) -> str:
+        """track how many times a particular URL was accessed"""
+        redis_client = redis.Redis()
+        redis_client.incr(f'count:{url}')
+        cached = redis_client.get(f'cached:{url}')
+        if cached:
+            return cached.decode('utf-8')
+        res = method(url)
+        redis_client.setex(f'cached:{url}', 10, res)
+        return res
+    return count
+
+
+@access
+def get_page(url: str) -> str:
+    """send request to url"""
+    return requests.get(url).text
+
+
+if __name__ == '__main__':
+    get_page('http://google.com')
